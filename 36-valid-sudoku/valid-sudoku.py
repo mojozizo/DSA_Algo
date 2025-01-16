a18ae37f4f5766703905a7db607966e0ecf5aa59
Jijo Valiyaveettil <37544499+jijovaliyaveettil@users.noreply.github.com>
2025-01-16 18:54:19 +0100

    Time: 7 ms (67.36%) | Memory: 12.4 MB (47.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,37 +4,22 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        for row in range(9):
-            row_set = set()
-            for i in range(9):
-                if board[row][i] == ".": 
-                    continue
-                if board[row][i] in row_set:
-                    return False
-                else:
-                    row_set.add(board[row][i])
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  
 
-        for col in range(9):
-            col_set = set()
-            for i in range(9):
-                if board[i][col] == ".": 
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
                     continue
-                if board[i][col] in col_set:
+                if ( board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                else:
-                    col_set.add(board[i][col])
 
-        for square in range(9):
-            square_set = set()
-            for i in range(3):
-                for j in range(3):
-                    row = (square//3) *3 + i
-                    col = (square%3)*3 + j
-                    if board[row][col] == ".": 
-                        continue
-                    if board[row][col] in square_set:
-                        return False
-                    else:
-                        square_set.add(board[row][col])
-        
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
+        
